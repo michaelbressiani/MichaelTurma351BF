ea4251fb028b14fee91ba8f317101512fe09d3d6
Michael Bressiani <>
2023-09-13 08:03:56 -0300

    Desafio Text Field - Tela de cadastro

---
+++ desafioTelaCadastroTurma351BF/desafioTelaCadastroTurma351BF/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  desafioTelaCadastroTurma351BF
+//
+//  Created by Michael Bressiani on 13/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var adressTextField: UITextField!
+    
+    @IBOutlet weak var registerChangeButton: UIButton!
+    
+    @IBOutlet weak var passordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configElements()
+    }
+
+    @IBAction func tapedRegisterButton(_ sender: UIButton) {
+    }
+    
+    func configElements() {
+        view.backgroundColor = UIColor.orange
+        nameTextField.placeholder = "Digite seu nome"
+        adressTextField.placeholder = "Digite seu endereço"
+        passordTextField.placeholder = "Digite sua senha"
+        registerChangeButton.setTitle("Cadastrar", for: .normal)
+        registerChangeButton.backgroundColor = UIColor.purple
+        
+        nameTextField.delegate = self
+        adressTextField.delegate = self
+        passordTextField.delegate = self
+    
+        registerChangeButton.isEnabled = false
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing")
+        textField.layer.borderWidth = 1.0
+        textField.layer.borderColor = UIColor.blue.cgColor
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
+        
+        textField.layer.borderWidth = 2.0
+        if textField.hasText {
+            textField.layer.borderColor = UIColor.lightGray.cgColor
+        } else {
+            textField.layer.borderColor = UIColor.red.cgColor
+        }
+        
+        if nameTextField.hasText && adressTextField.hasText && passordTextField.hasText {
+            registerChangeButton.isEnabled = true
+            print("Cadastro realizado com sucesso")
+        } else {
+            registerChangeButton.isEnabled = false
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        return true
+    }
+    
+}
+
